fd99f057581ec257de51b5d4dae3059bae2a44a5
KillerBot977 <collingreiss223@gmail.com>
2024-08-27 13:54:39 -0600

    builds

--- Build/Android/src/com/YourCompany/VRFinal/OBBData.java
+++ Build/Android/src/com/YourCompany/VRFinal/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-360034569L // the length of the file in bytes
+362678778L // the length of the file in bytes
 )
 };
 };
